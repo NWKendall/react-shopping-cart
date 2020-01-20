805eb2baf4b267783d2dc39edaa53c546be958f2
nicbongo <nicbongo@protonmail.com>
2020-01-20 13:58:53 -0400

    step 2 done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context
+import { ProductContext } from "./contexts/ProductContext";
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -15,6 +18,10 @@ function App() {
 		// add the given item to the cart
 		setCart([...cart, item])
 	};
+
+	const removeItem = item => {
+		setCart([]);
+	}
 
 	return (
 		<div className="App">
